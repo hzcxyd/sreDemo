125c9a91468e97252a3e07773f971491cfa94136
Dust <1522023659@qq.com>
2025-12-20 21:31:20 +0800

    push to nexus

---
+++ src/main/java/com/it/sredemo/SredemoApplication.java
@@ -0,0 +1,24 @@
+package com.it.sredemo;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SredemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SredemoApplication.class, args);
+    }
+
+    //prometheus show app name
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configure(
+        @Value("${spring.application.name}") String applicationName){
+        return (registry -> registry.config().commonTags("application",applicationName));
+    }
+
+}
---
+++ src/test/java/com/it/sredemo/SredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it.sredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SredemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
